cc5358a6f320be279867e73a34fd606be5fd24a7
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-05-02 13:10:00 -0700

    Netcdf46 (#13)
    * update: version 3.0
    
    * change ci_support recipe to libcdms which works
    
    * update to libnetcdf 4.6
    
    * update to libnetcdf 4.6
    
    * fix lints in recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
 export LFLAGS="-fPIC ${LFLAGS}"
 export FC=""
-
+export LDSHARED="$CC -shared -pthread" 
 ./configure --prefix=${PREFIX}
 ${PYTHON} setup.py install
 if [ `uname` == Darwin ]; then install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python @rpath/libpython2.7.dylib ${SP_DIR}/pycf/*.so ; fi
