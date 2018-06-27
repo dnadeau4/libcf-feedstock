997ed372a9db1b878b67799feb426ee9dfaf4019
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-06-27 14:38:02 -0700

    update recipe to gcc7

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,11 @@ export CXXLAGS="${CFLAGS} ${CXXLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
 export LFLAGS="-fPIC ${LFLAGS}"
+export LDSHARED="$CC -shared -pthread" 
 
 
 ./configure --prefix=${PREFIX}
-${PYTHON} setup.py install
+if [ `uname` == Linux ]; then
+    LDSHARED="$CC -shared -pthread"  ${PYTHON} setup.py install;
+fi
 if [ `uname` == Darwin ]; then install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python @rpath/libpython2.7.dylib ${SP_DIR}/pycf/*.so ; fi
